af18b8af96bfd554d58d07c2e22cd34ffe739222
burgueee <burguee16@gmail.com>
2024-08-13 20:23:17 -0300

    Primero

---
+++ MakeColor/MakeColor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MakeColor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MakeColor/MakeColor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MakeColor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
